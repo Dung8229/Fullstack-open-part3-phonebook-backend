22d0ffb4abc4a2272c14d090e4faf0f919ba4246
Dũng Quang <22028229@vnu.edu.vn>
2024-08-31 22:24:50 +0700

    Update #1

---
+++ index.js
@@ -0,0 +1,107 @@
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const app = express()
+
+app.use(morgan('tiny'))
+app.use(express.json())
+app.use(cors())
+
+let persons = 
+[
+  { 
+    "id": "1",
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": "2",
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": "3",
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": "4",
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
+]
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  const person = persons.find(person => person.id === id)
+  
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.get('/info', (request, response) => {
+  const personCount = persons.length
+  const currentTime = new Date().toString()
+
+  const info = `
+    <p>Phonebook has info for ${personCount} people</p>
+    <p>${currentTime}</p>
+  `
+
+  response.send(info)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
+})
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function isExistedName(name) {
+  return persons.some(person => person.name === name)
+}
+
+app.post('/api/persons', (request, response) => {
+  const person = request.body
+
+  if (!person.name) {
+    return response.status(400).json({ 
+      error: 'name missing' 
+    })
+  }
+
+  if (!person.number) {
+    return response.status(400).json({ 
+      error: 'number missing' 
+    })
+  }
+
+  if (isExistedName(person.name)) {
+    return response.status(400).json({ 
+      error: 'name must be unique' 
+    })
+  }
+
+  person.id = String(getRandomInt(1000))
+
+  persons = persons.concat(person)
+
+  response.json(person)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
